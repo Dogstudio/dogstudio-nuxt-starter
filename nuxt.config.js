03763050c91c8c64043d2656a76c8025a57b2f9b
Anthony Du Pont <bulldog@dogstudio.be>
2021-12-09 11:29:13 +0100

    :construction: Build configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,1 +1,226 @@
-// Silence is golden.
+export default {
+  // Change default Nuxt SSR value
+  ssr: true,
+
+  // Change default Nuxt target
+  target: 'server',
+
+  // Define default meta for the application
+  // See: https://nuxtjs.org/docs/configuration-glossary/configuration-head
+  head: {
+    title: 'Dogstudio | Nuxt Starter',
+    meta: [
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover, user-scalable=no',
+      },
+      {
+        hid: 'title',
+        name: 'title',
+        content: 'Dogstudio | Nuxt Starter',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Opinionated Nuxt starter by Dogstudio',
+      },
+
+      // Open Graph / Facebook
+      {
+        hid: 'og:type',
+        content: 'website',
+        property: 'og:type',
+      },
+      {
+        hid: 'og:url',
+        content: 'http://localhost:3000',
+        property: 'og:url',
+      },
+      {
+        hid: 'og:title',
+        content: 'Dogstudio | Nuxt Starter',
+        property: 'og:title',
+      },
+      {
+        hid: 'og:image',
+        content: '/assets/images/share.png',
+        property: 'og:image',
+      },
+      {
+        hid: 'og:description',
+        content: 'Opinionated Nuxt starter by Dogstudio',
+        property: 'og:description',
+      },
+
+      // Twitter
+      {
+        hid: 'twitter:url',
+        content: 'http://localhost:3000',
+        property: 'twitter:url',
+      },
+      {
+        hid: 'twitter:card',
+        content: 'summary_large_image',
+        property: 'twitter:card',
+      },
+      {
+        hid: 'twitter:title',
+        content: 'Dogstudio | Nuxt Starter',
+        property: 'twitter:title',
+      },
+      {
+        hid: 'twitter:image',
+        content: '/assets/images/share.png',
+        property: 'twitter:image',
+      },
+      {
+        hid: 'twitter:description',
+        content: 'Opinionated Nuxt starter by Dogstudio',
+        property: 'twitter:description',
+      },
+    ],
+
+    // Links
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon.ico', 
+      },
+    ],
+
+    // Scripts
+    script: [
+      {
+        src: '/vendors/modernizr.js',
+        type: 'text/javascript',
+        async: true,
+      }
+    ],
+  },
+
+  // Change the default Nuxt mode
+  mode: 'universal',
+
+  // Define custom directories for the application
+  // See: https://nuxtjs.org/docs/configuration-glossary/configuration-dir
+  dir: {
+    // Static files
+    static: 'public',
+
+    // Source files
+    app: 'src/app',
+    store: 'src/store',
+    assets: 'src/assets',
+    layouts: 'src/layouts',
+    middleware: 'src/middleware',
+  },
+
+  // Global CSS files includes in every page
+  // See: https://nuxtjs.org/docs/configuration-glossary/configuration-css
+  css: [
+    '~/src/styles/global.scss',
+  ],
+
+  // Toggle the loading indicators between the routes or customize them
+  // See: https://nuxtjs.org/docs/configuration-glossary/configuration-loading
+  // See: https://nuxtjs.org/docs/configuration-glossary/configuration-loading-indicator
+  loading: false,
+  loadingIndicator: false,
+
+  // Enable Nuxt plugins from the `plugins` directory
+  // See: https://nuxtjs.org/docs/configuration-glossary/configuration-plugins
+  plugins: [],
+
+  // Enable Nuxt modules to extend its core functionalities
+  // See: https://nuxtjs.org/docs/configuration-glossary/configuration-modules
+  modules: [],
+
+  // Enable auto-import of components within pages, layouts and other components
+  // See: https://nuxtjs.org/docs/configuration-glossary/configuration-components
+  components: [
+    { path: '~/src/components', extensions: ['vue'] },
+  ],
+
+  // Enable / Disable telemetry
+  telemetry: false,
+
+  // Customize Webpack configuration
+  // See: https://nuxtjs.org/docs/configuration-glossary/configuration-build
+  build: {
+    // Add Webpack plugins
+    plugins: [],
+
+    // Customize options for integrated Webpack loaders
+    loaders: {
+      vue: {
+        // See: https://nuxtjs.org/docs/features/configuration#extend-webpack-to-load-audio-files
+        transformAssetUrls: {
+          audio: 'src',
+        },
+      },
+      imgUrl: {
+        limit: false,
+      },
+      fontUrl: {
+        limit: false,
+      }
+    },
+
+    // Enable CSS chunks extraction in files
+    extractCSS: {
+      ignoreOrder: true,
+    },
+
+    // Extend Webpack configuration
+    extend(config) {
+      // New Webpack rules
+      const rules = [
+        // Audio files
+        // See: https://nuxtjs.org/docs/features/configuration#extend-webpack-to-load-audio-files
+        {
+          test: /\.(ogg|mp3|wav|mpe?g)$/i,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        },
+
+        // Shaders
+        {
+          test: /\.(glsl|frag|vert)$/,
+          exclude: /node_modules/,
+          use: [
+            'raw-loader',
+            {
+              loader: 'glslify-loader',
+              options: {
+                transform: ['glslify-import'],
+              },
+            },
+          ],
+        },
+      ];
+
+      // Add Webpack rules
+      for (const rule of rules) {
+        config.module.rules.push(rule);
+      }
+
+      // Resolve Extensions
+      config.resolve.extensions.push('.css');
+      config.resolve.extensions.push('.sass');
+      config.resolve.extensions.push('.scss');
+      config.resolve.extensions.push('.json');
+
+      // Resolve Modules
+      config.resolve.modules.push('.');
+      config.resolve.modules.push('src');
+    }
+  }
+}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ import UAParser from 'ua-parser-js';
 
 // Server vs Browser
 // See: https://github.com/vercel/next.js/issues/5354#issuecomment-520305040
-export const IS_SERVER = !process.client;
+export const IS_SERVER = process.server;
 export const IS_BROWSER = process.client;
 
 // Environment Constants
