3178b47ed2ad9a0bd97280177f37a9eb65b5bdeb
Anthony Du Pont <bulldog@dogstudio.be>
2022-03-08 13:44:03 +0100

    :sparkles: Add Icon component

---
+++ src/components/elements/Icon/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <span
+    class="svg"
+    :class="`svg--${name}`"
+    :style="{ width: `${width}px`, height: `${height}px` }"
+  >
+    <component :is="iconComponent" :width="width" :height="height" />
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'IconElement',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: Number,
+      required: true,
+    },
+    height: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    iconComponent() {
+      return () => import(`./Icons/icon-${this.name}`)
+    },
+  },
+}
+</script>
