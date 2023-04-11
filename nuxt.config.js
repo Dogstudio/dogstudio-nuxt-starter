851226cb2c555480bc9382e004b39f93b35c8451
Anthony Du Pont <bulldog@dogstudio.be>
2023-04-11 09:35:54 +0200

    :construction: Change nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default defineNuxtConfig({
           property: 'og:type',
         },
         {
-          content: process.env.NUXT_PUBLIC_PUBLIC_PATH,
+          content: process.env.NUXT_PUBLIC_PUBLIC_PATH || 'http://localhost:3000/',
           property: 'og:url',
         },
         {
@@ -61,7 +61,7 @@ export default defineNuxtConfig({
 
         // Twitter
         {
-          content: process.env.NUXT_PUBLIC_PUBLIC_PATH,
+          content: process.env.NUXT_PUBLIC_PUBLIC_PATH || 'http://localhost:3000/',
           property: 'twitter:url',
         },
         {
