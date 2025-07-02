3b52f00e1b5b565598c1018e75f4baf3e0c15be1
SoufianeLasri <soufiane.lasri@gmail.com>
2025-07-02 16:42:16 +0200

    Clean

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,15 @@
 <script setup>
-import debounce from "@/utils/debounce";
+import debounce from '~/utils/debounce'
 
 // Constants
-import { DEV } from "@/utils/constants";
+import { DEV } from '~/utils/constants'
 
 // Variables
-const route = useRoute();
-const enable = route.query.debug === "grid";
+const route = useRoute()
+const enable = route.query.debug === 'grid'
 
 // i18n / SEO
-const i18nHead = useLocaleHead();
+const i18nHead = useLocaleHead()
 useHead(() => ({
   htmlAttrs: {
     lang: i18nHead.value.htmlAttrs?.lang,
@@ -17,29 +17,29 @@ useHead(() => ({
   },
   link: [...(i18nHead.value.link || [])],
   meta: [...(i18nHead.value.meta || [])],
-}));
+}))
 
 // State
-const showGrid = useState(() => DEV && enable);
+const showGrid = useState(() => DEV && enable)
 
 // Methods
 const handleResize = () => {
-  const _root = document.querySelector(":root");
-  const svh = document.querySelector("#svh");
-  const lvh = document.querySelector("#lvh");
+  const _root = document.querySelector(':root')
+  const svh = document.querySelector('#svh')
+  const lvh = document.querySelector('#lvh')
 
-  _root.style.setProperty("--svh", `${svh.clientHeight}px`);
-  _root.style.setProperty("--lvh", `${lvh.clientHeight}px`);
-};
+  _root.style.setProperty('--svh', `${svh.clientHeight}px`)
+  _root.style.setProperty('--lvh', `${lvh.clientHeight}px`)
+}
 
 // Lifecycle
 onMounted(() => {
   // Forget the scroll position on refresh
-  history.scrollRestoration = "manual";
+  history.scrollRestoration = 'manual'
 
-  handleResize();
-  window.addEventListener("resize", debounce(handleResize, 100));
-});
+  handleResize()
+  window.addEventListener('resize', debounce(handleResize, 100))
+})
 </script>
 
 <template>
